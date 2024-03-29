bed36a7263bf2fe41b23a62e53358d40831cd5ff
gauravPalKsolves <gaurav.pal@ksolves.com>
2024-03-29 18:21:37 +0530

    Added MOCHA AWESOME HTML Report

--- cypress/e2e/Login_Customer.cy.js
+++ cypress/e2e/Login_Customer.cy.js
@@ -1,25 +1,24 @@
 /// <reference types="cypress" />
 
+// Importing the LoginCustomer class for interacting with login elements
+import { LoginCustomer } from "../Selectors/Customer_login_CSS";
+const Login = new LoginCustomer();
 
-import { LoginCustomer } from "../Selectors/Customer_login_CSS"
-const Login = new LoginCustomer()
-
-import cred from '../fixtures/Login_cred_customer.json'
-
+// Importing credentials from fixture file for testing
+import cred from '../fixtures/Login_cred_customer.json';
 
 describe('Login the customer', () => {
     beforeEach(() => {
-        Login.OpenTheURL()
-    })
-
+        Login.OpenTheURL();
+    });
 
     it('Login the user with correct credentials', () => {
-
-        Login.Email_n_Password(cred[0].Client.Email, cred[0].Client.Password)
-        cy.url().should('include', 'dashboard')
-    })
+        Login.Email_n_Password(cred[0].Client.Email, cred[0].Client.Password);
+        cy.url().should('include', 'dashboard');
+    });
 
     it('Login the user with wrong credentials', () => {
-        Login.Email_n_Password(cred[1].Invalid.Email, cred[1].Invalid.Password)
-    })
-})
+        Login.Email_n_Password(cred[1].Invalid.Email, cred[1].Invalid.Password);
+        cy.contains('Invalid Credentials..!!').should('be.visible').and('have.text', 'Invalid Credentials..!!');
+    });
+});
